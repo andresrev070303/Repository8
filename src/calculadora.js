44b5d12b17ac2cd58c66da937b0f9679cbed692f
Andres Revollo <andres.revollo@usb.edu.bo>
2024-05-15 21:32:15 -0400

    Se refactorizo el codigo separandolo en funciones para mejor lectura y comprension del codigo

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,23 +1,43 @@
 function sumarValoresCadena(cadena) {
-    if (Array.isArray(cadena)) {
+  if (Array.isArray(cadena)) {
       return [];
-    }
-    if (!cadena.includes(" ") && !cadena.includes(",") && !cadena.includes("-") && !cadena.includes(";")) {
+  }
+  if (esNumero(cadena)) {
       return parseInt(cadena);
-    }
-    let separador = ",";
-    if (cadena.startsWith("//[")) {
+  }
+  const separador = obtenerSeparador(cadena);
+  const valores = dividirCadena(cadena, separador).map(Number);
+  const numerosPorDebajoDeLimite = filtrarNumerosPorLimite(valores);
+  return sumarNumeros(numerosPorDebajoDeLimite);
+}
+
+function esNumero(cadena) {
+  return !cadena.includes(" ") && !cadena.includes(",") && !cadena.includes("-") && !cadena.includes(";");
+}
+
+function obtenerSeparador(cadena) {
+  let separador = ",";
+  if (cadena.startsWith("//[")) {
       const finSeparador = cadena.indexOf("]");
       separador = cadena.substring(3, finSeparador);
-      cadena = cadena.substring(finSeparador + 2);
-    }
-    const valores = dividirCadena(cadena, separador).map(Number);
-    const numerosPorDebajoDeLimite = valores.filter(numero => numero <= 1000);
-    return numerosPorDebajoDeLimite.reduce((total, valor) => total + valor, 0);
+  }
+  return separador;
 }
 
 function dividirCadena(cadena, separador) {
-    return cadena.split(new RegExp(`[ ,\\-${separador}]+`));
+  if (cadena.startsWith("//[")) {
+      const finDelimitador = cadena.indexOf("]");
+      cadena = cadena.substring(finDelimitador + 2);
+  }
+  return cadena.split(new RegExp(`[ ,\\-${separador}]+`));
+}
+
+function filtrarNumerosPorLimite(valores) {
+  return valores.filter(numero => numero <= 1000);
+}
+
+function sumarNumeros(valores) {
+  return valores.reduce((total, valor) => total + valor, 0);
 }
 
 export default sumarValoresCadena;
