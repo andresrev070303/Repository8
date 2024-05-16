a2bf5c87cac42766d8be943d4b2617145fb64350
Andres Revollo <andres.revollo@usb.edu.bo>
2024-05-15 21:42:50 -0400

    Se corrigio una prueba mal planteada

--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -2,7 +2,7 @@ import calcularCadena from "./calculadora.js";
 
 describe("calcular", () => {
   it("debería devolver una cadena vacia", () => {
-    expect(calcularCadena([])).toEqual([]);
+    expect(calcularCadena(" ")).toEqual(0);
   });
   it("debería devolver el valor del elemento de la cadena ", () => {
     expect(calcularCadena("5")).toEqual(5);
